34c8c76b058b0761015c80e9b8d759e50f1d0c44
Arkelis <guillaume.fayard@pycolore.fr>
2019-10-06 23:54:23 +0200

    (Enhance) Autoplay

--- sunflower/server.py
+++ sunflower/server.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    metadata = radio.fetch(radio.get_current_station())
+    current_station = radio.get_current_station()
+    metadata = radio.fetch(current_station)
     if metadata["station"] == "RTL 2":
         if metadata["type"] == "Musique":
             title = metadata["artist"] + " &bull; " + metadata["title"]
@@ -15,7 +16,8 @@ def index():
     elif "France " in metadata["station"]:
         title = metadata["diffusion_title"]
         metadata["thumbnail_src"] = "https://upload.wikimedia.org/wikipedia/fr/thumb/8/8d/France_inter_2005_logo.svg/1024px-France_inter_2005_logo.svg.png"
-    return render_template("radio.html", card_title=title, metadata=metadata)
+    flux_url = radio.FLUX_URL.get(current_station)
+    return render_template("radio.html", card_title=title, metadata=metadata, flux_url=flux_url)
 
 @app.route("/on-air")
 def current_show_data():
---
+++ sunflower/static/app.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    document.querySelector("audio").play()
+};
