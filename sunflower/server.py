698d36e1e1b498cf2feb5145d65d1ee31866cb80
Arkelis <guillaume.fayard@pycolore.fr>
2019-10-23 22:04:47 +0200

    (Enhance) Update info url not hard coded anymore

--- sunflower/server.py
+++ sunflower/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, url_for
+from flask import Flask, jsonify, render_template, url_for, request
 from flask_cors import CORS, cross_origin
 import time
 
@@ -17,7 +17,8 @@ def _prepare_broadcast_info():
 @app.route("/")
 def index():
     title, metadata, flux_url = _prepare_broadcast_info()
-    return render_template("radio.html", card_title=title, metadata=metadata, flux_url=flux_url)
+    update_url = request.url_root + "update"
+    return render_template("radio.html", card_title=title, metadata=metadata, flux_url=flux_url, update_url=update_url)
 
 @app.route("/update")
 def update_broadcast_info():
