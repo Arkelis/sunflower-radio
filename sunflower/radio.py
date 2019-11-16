59c5ef08351044683931ad788b7b996cc578b821
Guillaume Fayard <guillaume2.fayard@wanadoo.fr>
2019-11-16 18:56:20 +0100

    (Refactor) watcher in watcher.py and not in Radio

--- sunflower/radio.py
+++ sunflower/radio.py
@@ -154,7 +154,7 @@ class Radio(RedisMixin):
             }
         return metadata, info
 
-    def _process_radio(self):
+    def process_radio(self):
         """Fetch metadata, and if needed do some treatment.
         
         Treatments:
@@ -166,51 +166,4 @@ class Radio(RedisMixin):
         self.current_broadcast_metadata = metadata
         self.current_broadcast_info = info
 
-    def watch(self):
-        """Update metadata if needed. This is launched on separated thread."""
-        import logging
-        import logging.handlers
-
-        # instanciate logger
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG)
-
-        # format msg
-        formatter = logging.Formatter("[%(asctime)s] %(levelname)s :: %(message)s")
-
-        # rotate
-        file_handler = logging.handlers.RotatingFileHandler("/tmp/watcher.log", "a", 1000000, 1)
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-        logger.debug("Starting watcher.")
-
-        if self.current_broadcast_metadata is None:
-            self.current_broadcast_metadata = self.get_current_broadcast_metadata()
-
-        # loop
-        while True:
-            if datetime.now().timestamp() > self.current_broadcast_metadata["end"]:
-                logger.debug("Processing. Current timestamp is {}".format(datetime.now().timestamp()))
-                logger.debug("Before processing, metadata is {}".format(self.current_broadcast_metadata))
-                try:
-                    self._process_radio()
-                except Exception as err:
-                    import traceback
-                    logger.error("Une erreur est survenue pendant la mise à jour des données.")
-                    logger.error(traceback.format_exc())
-                    metadata = {
-                        "message": "An error occured when fetching data.",
-                        "type": "Erreur",
-                        "end": 0,
-                    }
-                    info = {
-                        "current_thumbnail": self.current_station.station_thumbnail,
-                        "current_station": self.current_station.station_name,
-                        "current_broadcast_title": "Une erreur est survenue",
-                        "current_show_title": "Erreur interne du serveur",
-                        "current_broadcast_summary": "Une erreur est survenue pendant la récupération des métadonnées . Si cette erreur n'est pas bloquante, les données ont une chance de se mettre à jour automatiquement.",
-                        "current_broadcast_end": 0,
-                    }
-                logger.debug("After processing, metadata is {}".format(self.current_broadcast_metadata))
-            sleep(1)
+    
--- sunflower/watcher.py
+++ sunflower/watcher.py
@@ -1,11 +1,67 @@
-from daemonize import Daemonize
+import logging
+import logging.handlers
 import os
 import sys
+from datetime import datetime
+from time import sleep
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+if __name__ == "__main__":
+    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from radio import Radio
+from daemonize import Daemonize
+from sunflower.radio import Radio
 
-pid = "/tmp/sunflower-radio-watcher.pid"
-daemon = Daemonize(app="sunflower-radio-watcher", pid=pid, action=Radio().watch)
-daemon.start()
+def watch():
+    """Keep data for radio client up to date."""
+    # instanciate radio
+    radio = Radio()
+
+    # instanciate logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # format msg
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)s :: %(message)s")
+
+    # rotate
+    file_handler = logging.handlers.RotatingFileHandler("/tmp/watcher.log", "a", 1000000, 1)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.debug("Starting watcher.")
+
+    if radio.current_broadcast_metadata is None:
+        radio.current_broadcast_metadata = radio.get_current_broadcast_metadata()
+
+    # loop
+    while True:
+        if datetime.now().timestamp() > radio.current_broadcast_metadata["end"]:
+            logger.debug("Processing. Current timestamp is {}".format(datetime.now().timestamp()))
+            logger.debug("Before processing, metadata is {}".format(radio.current_broadcast_metadata))
+            try:
+                radio.process_radio()
+            except Exception as err:
+                import traceback
+                logger.error("Une erreur est survenue pendant la mise à jour des données.")
+                logger.error(traceback.format_exc())
+                metadata = {
+                    "message": "An error occured when fetching data.",
+                    "type": "Erreur",
+                    "end": 0,
+                }
+                info = {
+                    "current_thumbnail": radio.current_station.station_thumbnail,
+                    "current_station": radio.current_station.station_name,
+                    "current_broadcast_title": "Une erreur est survenue",
+                    "current_show_title": "Erreur interne du serveur",
+                    "current_broadcast_summary": "Une erreur est survenue pendant la récupération des métadonnées . Si cette erreur n'est pas bloquante, les données ont une chance de se mettre à jour automatiquement.",
+                    "current_broadcast_end": 0,
+                }
+            logger.debug("After processing, metadata is {}".format(radio.current_broadcast_metadata))
+        sleep(5)
+
+
+if __name__ == "__main__":
+    pid = "/tmp/sunflower-radio-watcher.pid"
+    daemon = Daemonize(app="sunflower-radio-watcher", pid=pid, action=watch)
+    daemon.start()
