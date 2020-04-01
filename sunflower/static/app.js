614cfe5fb69bcca609c05a874d5bf771900ff230
Arkelis <guillaume.fayard@pycolore.fr>
2020-04-01 19:27:15 +0200

    Better responsive design

--- sunflower/static/app.js
+++ sunflower/static/app.js
@@ -2,38 +2,68 @@ const updateUrl = document.getElementById("info-update").attributes["data-update
 const eventsUrl = document.getElementById("info-update").attributes["data-listen-url"].value
 
 class FlippedElement {
+    x
     y
+    h
+    w
     element
 
     get newY() {
         return this.element.getBoundingClientRect().y
     }
 
+    get newX() {
+        return this.element.getBoundingClientRect().x
+    }
+
+    get newH() {
+        return this.element.getBoundingClientRect().height
+    }
+
+    get newW() {
+        return this.element.getBoundingClientRect().width
+    }
+
+
     constructor(element) {
         this.element = document.querySelector(element)
         this.y = this.newY
+        this.x = this.newX
+        this.h = this.newH
+        this.w = this.newW
+    }
+
+    read() {
+        this.x = this.newX
+        this.y = this.newY
+        this.h = this.newH
+        this.w = this.newW
     }
 
     flip() {
         const newY = this.newY
+        const newX = this.newX
+        const newH = this.newH
+        const newW = this.newW
         const deltaY = this.y - newY
-        this.y = newY
-        console.log("flipped")
+        const deltaX = this.x - newX
+        const ratioH = this.w / newW
+        const ratioW = this.h / newH
         this.element.animate(
             [
                 {
-                    transform: `translateY(${deltaY}px)`
+                    transform: `translate(${deltaX}px, ${deltaY}px) scale(${ratioW}, ${ratioH})`
                 },
                 {
                     transform: "none"
                 },
             ],
             {
-                duration: 5 * Math.abs(deltaY),
+                duration: Math.max(400, 3 * Math.abs(deltaY)),
                 fill: "both",
                 easing: "ease-in-out",
-            })
-        //this.element.style.transform = `translateY(${deltaY}px)`
+            }
+        )
     }
 }
 
@@ -46,6 +76,7 @@ let audioPlayer = new FlippedElement("audio")
  */
 function updateCardInfos(divsToUpdate, thumbnailNode=null) {
     // update info
+    audioPlayer.read()
     divsToUpdate.forEach((element, i) => {
         element[0].innerText = element[1]
     })
@@ -95,10 +126,12 @@ function updateCardBody() {
             })
 
             divsToUpdate.forEach((element, i) => { 
-                setTimeout(() => {
-                    element[0].classList.remove("fade-in")
-                    element[0].classList.add("fade-out")
-                }, 100*i)
+                if (element.innerHTML !== "") {
+                    setTimeout(() => {
+                        element[0].classList.remove("fade-in")
+                        element[0].classList.add("fade-out")
+                    }, 100*i)
+                }
             })
 
             // check thumbnail src
@@ -126,3 +159,18 @@ es.onerror = err => console.log(err)
 
 console.log("hello")
 updateCardBody()
+
+// Toggle channels list
+
+const channelsListHead = document.querySelector(".channels-list-head")
+const channelsListChevron = document.querySelector(".hide-channels-list")
+const channelsList = document.querySelector(".channels-list")
+
+channelsListHead.onclick = () => { channelsList.classList.add("show") }
+channelsListChevron.onclick = () => { channelsList.classList.remove("show")}
+
+// Toggle cover size
+
+document.querySelector("#current-thumbnail-container").onclick = () => {
+    document.querySelector("body").classList.toggle("big-cover");
+}
