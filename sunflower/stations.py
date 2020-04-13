a204c57d41c5dd28aad0988c033c9137578a014b
Arkelis <guillaume.fayard@pycolore.fr>
2020-04-13 09:39:17 +0200

    Fix computing of max_length

--- sunflower/stations.py
+++ sunflower/stations.py
@@ -448,7 +448,8 @@ class PycolorePlaylistStation(Station):
         now = datetime.now()
         if self._current_song_end - 10 < int(now.timestamp()):
             delay = max(self._current_song_end - now, 0)
-            max_length = (channel.get_station_info(now.time())[1] - now.time()).seconds - delay
+            end_of_current_station = datetime.combine(date.today(), channel.get_station_info(now.time())[1])
+            max_length = (end_of_current_station - now).seconds - delay
             self._play(delay, max_length)
 
     @classmethod
