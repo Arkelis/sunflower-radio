e91288fbb79d52bbd1ef9c3ed3307f05bc9e3cbb
Guillaume Fayard <guillaume2.fayard@wanadoo.fr>
2020-04-14 10:12:09 +0200

    Change songs path pattern

--- sunflower/settings.py
+++ sunflower/settings.py
@@ -1,6 +1,6 @@
 ICECAST_SERVER_URL = "https://icecast.pycolore.fr/"
 
-BACKUP_SONGS_GLOB_PATTERN = "/home/guillaume/radio/songs/*.ogg"
+BACKUP_SONGS_GLOB_PATTERN = "/home/guillaume/radio/songs/*.opus"
 
 CHANNELS = [
     "tournesol",
