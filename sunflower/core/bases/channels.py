2b94a2c853a40fb930777914116f8c4d6335647d
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2020-06-08 13:32:13 +0200

    Fix metadata sending

--- sunflower/core/bases/channels.py
+++ sunflower/core/bases/channels.py
@@ -238,8 +238,8 @@ class Channel(RedisMixin):
             logger.debug(f"channel={self.endpoint} StreamMetadata is empty")
             return
         session = telnetlib.Telnet("localhost", 1234)
-        session.write(f'{self.endpoint}.insert title="{new_stream_metadata.title}",'.encode())
-        session.write(f'artist="{new_stream_metadata.artist}",album="{new_stream_metadata.album}\n'.encode())
+        metadata_string = f'title="{new_stream_metadata.title}",artist="{new_stream_metadata.artist}",album="{new_stream_metadata.album}"'
+        session.write(f'{self.endpoint}.insert {metadata_string}\n'.replace("-", " ").encode())
         session.write("exit\n".encode())
         session.close()
         logger.debug(f"channel={self.endpoint} {new_stream_metadata} sent to liquidsoap")
