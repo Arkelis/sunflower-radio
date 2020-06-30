5a71983c7e035d003c5e4a5655ebc14472a8a94f
Arkelis <guillaume.fayard@pycolore.fr>
2020-06-30 16:24:28 +0200

    Update tests

--- tests/test_channels.py
+++ tests/test_channels.py
@@ -1,11 +1,13 @@
 from sunflower.channels import tournesol, music
-from sunflower.stations import FranceMusique, FranceInter, FranceInfo, FranceCulture, RTL2, PycolorePlaylistStation
+from sunflower.stations import FranceMusique, FranceInter, FranceInfo, FranceCulture, RTL, RTL2, PycolorePlaylistStation
 from collections import Counter
+
 
 def test_tournesol_station_parsing():
     # Counter([...]) == Counter([...]) permet de comparer les éléments et leurs occurrences
     # de deux itérables sans se soucier de l'ordre
     assert Counter(tournesol.stations) == Counter((FranceCulture, FranceInfo, FranceInter, FranceMusique, RTL2, PycolorePlaylistStation))
 
+
 def test_music_station_parsing():
-    assert Counter(music.stations) == Counter((RTL2, PycolorePlaylistStation))
+    assert Counter(music.stations) == Counter((RTL2, RTL, PycolorePlaylistStation))
