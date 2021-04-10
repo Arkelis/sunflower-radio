a1ba9f5c604d9d241c4703bfeb815555859f7040
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2021-04-10 23:49:42 +0200

    Remove check_obj_integrity not used anymore

--- sunflower/core/functions.py
+++ sunflower/core/functions.py
@@ -3,27 +3,6 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
-from sunflower.core.persistence import PersistentAttribute
-
-
-def check_obj_integrity(obj):
-    """Perfom several checks in order to prevent some runtime errors."""
-    
-    errors = []
-
-    # 1. If obj has PersistentAttribute attributes, check if this object
-    #    has the 'data_type' and 'endpoint' attributes.
-
-    for attr in vars(type(obj)).values():
-        if not isinstance(attr, PersistentAttribute):
-            continue
-        if not hasattr(obj, "endpoint"):
-            errors.append(f"Missing 'endpoint' attribute in {obj} which contains PersistentAttribute descriptor.")
-        if not hasattr(obj, "data_type"):
-            errors.append(f"Missing 'data_type' attribute in {obj} which contains PersistentAttribute descriptor.")
-
-    return errors
-
 
 def run_coroutine_synchronously(coroutine_function, *args, **kwargs):
     """Run coroutine syncronously.
--- sunflower/scheduler.py
+++ sunflower/scheduler.py
@@ -8,10 +8,8 @@ import traceback
 if __name__ == "__main__":
     sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from daemonize import Daemonize
 from sunflower.core.scheduler import Scheduler
 from sunflower.channels import tournesol, music
-from sunflower.core.functions import check_obj_integrity
 
 
 def launch_scheduler():
@@ -23,40 +21,24 @@ def launch_scheduler():
     formatter = logging.Formatter("[%(asctime)s] %(levelname)s :: %(message)s")
     
     # rotate
-    file_handler = logging.handlers.RotatingFileHandler("/tmp/beta-scheduler.log", "a", 1000000, 1)
+    file_handler = logging.handlers.RotatingFileHandler("/tmp/sunflower-scheduler.log", "a", 1000000, 1)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
-    
-    logger.info(f"Checking objects integrity.")
-
-    scheduled_channels = [music, tournesol]
-    check_errors = {}
-    for channel in scheduled_channels:
-        if errors := check_obj_integrity(channel):
-            check_errors[str(channel)] = errors
-        for station in channel.stations:
-            if errors := check_obj_integrity(station):
-                check_errors[str(station)] = errors
-
-    if check_errors:
-        for obj, errors in check_errors.items():
-            logger.error(f"Errors for object {obj}:" + "\n" + "\n".join(f"- {err}" for err in errors))
-        logger.info("Programme stopped.")
-        raise RuntimeError("Integrity errors found.")
 
     logger.info("Starting scheduler.")
     try:
-        scheduler = Scheduler(scheduled_channels, logger)
+        scheduler = Scheduler([music, tournesol], logger)
+        logger.info("Scheduler instantiated.")
+        scheduler.run()
     except Exception as err:
         std_handler = logging.StreamHandler(sys.stdout)
         std_handler.setLevel(logging.ERROR)
         std_handler.setFormatter(logging.Formatter("%(message)s"))
         logger.addHandler(std_handler)
         logger.error(traceback.format_exc())
-    logger.info("Scheduler instantiated.")
-    
-    scheduler.run()
+
+
 
 
 if __name__ == "__main__":
