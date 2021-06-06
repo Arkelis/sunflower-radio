20b9880d2f9cb00edbe335fcef558b355ebd2592
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2021-06-06 15:12:35 +0200

    Make Repository async

--- sunflower/core/repository.py
+++ sunflower/core/repository.py
@@ -7,20 +7,19 @@ from typing import Optional
 from typing import Type
 
 import aredis
-from sunflower.core.functions import run_coroutine_synchronously
 
 
 class Repository(ABC):
     @abstractmethod
-    def retrieve(self, key: str, object_hook: Optional[Callable] = None):
+    async def retrieve(self, key: str, object_hook: Optional[Callable] = None):
         ...
 
     @abstractmethod
-    def persist(self, key: str, value: Any, json_encoder_cls: Optional[Type[json.JSONEncoder]] = None):
+    async def persist(self, key: str, value: Any, json_encoder_cls: Optional[Type[json.JSONEncoder]] = None):
         ...
 
     @abstractmethod
-    def publish(self, key: str, channel, data):
+    async def publish(self, key: str, channel, data):
         ...
 
 
@@ -35,26 +34,26 @@ class RedisRepository(Repository):
     def __init__(self, *args, **kwargs):
         self._redis = aredis.StrictRedis()
 
-    def retrieve(self, key: str, object_hook: Optional[Callable] = None):
+    async def retrieve(self, key: str, object_hook: Optional[Callable] = None):
         """Get value for given key from Redis.
 
         Data got from Redis is loaded from json with given object_hook.
         If no data is found, return None.
         """
-        raw_data = run_coroutine_synchronously(self._redis.get, key)
+        raw_data = await self._redis.get(key)
         if raw_data is None:
             return None
         return json.loads(raw_data.decode(), object_hook=object_hook)
 
-    def persist(self, key: str, value: Any, json_encoder_cls: Optional[Type[json.JSONEncoder]] = None):
+    async def persist(self, key: str, value: Any, json_encoder_cls: Optional[Type[json.JSONEncoder]] = None):
         """Set new value for given key in Redis.
 
         value is dumped as json with given json_encoder_cls.
         """
         json_data = json.dumps(value, cls=json_encoder_cls)
-        return run_coroutine_synchronously(self._redis.set, key, json_data)
+        return await self._redis.set(key, json_data)
 
-    def publish(self, channel, data):
+    async def publish(self, channel, data):
         """publish a message to a redis channel.
 
         Parameters:
@@ -65,4 +64,4 @@ class RedisRepository(Repository):
         """
         if not isinstance(data, str):
             data = json.dumps(data)
-        run_coroutine_synchronously(self._redis.publish, channel, data)
+        await self._redis.publish(channel, data)
